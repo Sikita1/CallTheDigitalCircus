1fe0c7f18de2796de26d0bb05b9e52fadb2bdc11
Sikita1 <ns310893@mail.ru>
2023-11-16 12:41:34 +0300

    11/16/2023

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using YG;
@@ -17,6 +16,15 @@ public class Controller : MonoBehaviour
     [SerializeField] private AudioClip[] _audioPomni;
     [SerializeField] private AudioClip[] _audioRagatha;
     [SerializeField] private AudioClip[] _audioZooble;
+    [SerializeField] private AudioClip[] _audioHorror;
+
+    [SerializeField] private Image _caineHorror;
+    [SerializeField] private Image _gangleHorror;
+    [SerializeField] private Image _jaxHorror;
+    [SerializeField] private Image _kingerHorror;
+    [SerializeField] private Image _pomniHorror;
+    [SerializeField] private Image _ragathaHorror;
+    [SerializeField] private Image _zoobleHorror;
 
     private AudioSource _audioSource;
     private int _countADS;
@@ -34,85 +42,155 @@ public class Controller : MonoBehaviour
 
     public void ButtonClickCaine(Image image)
     {
-        _imagePanel.sprite = image.sprite;
-        _allPersons.gameObject.SetActive(false);
-        _panel.gameObject.SetActive(true);
-        _audioSource.clip = GetRundomAudio(_audioCaine);
-        _audioSource.Play();
-        _countADS++;
+        if (IsChanceHorror())
+        {
+            ShowHorrorScene(_caineHorror);
+        }
+        else
+        {
+            ShowCharacterImage(image);
+            PlayAudioCharacter(_audioCaine);
+        }
+
+        _counterADS();
     }
 
     public void ButtonClickGangle(Image image)
     {
-        _imagePanel.sprite = image.sprite;
-        _allPersons.gameObject.SetActive(false);
-        _panel.gameObject.SetActive(true);
-        _audioSource.clip = GetRundomAudio(_audioGangle);
-        _audioSource.Play();
-        _countADS++;
+        if (IsChanceHorror())
+        {
+            ShowHorrorScene(_gangleHorror);
+        }
+        else
+        {
+            ShowCharacterImage(image);
+            PlayAudioCharacter(_audioGangle);
+        }
+
+        _counterADS();
     }
 
     public void ButtonClickJax(Image image)
     {
-        _imagePanel.sprite = image.sprite;
-        _allPersons.gameObject.SetActive(false);
-        _panel.gameObject.SetActive(true);
-        _audioSource.clip = GetRundomAudio(_audioJax);
-        _audioSource.Play();
-        _countADS++;
+        if (IsChanceHorror())
+        {
+            ShowHorrorScene(_jaxHorror);
+        }
+        else
+        {
+            ShowCharacterImage(image);
+            PlayAudioCharacter(_audioJax);
+        }
+
+        _counterADS();
     }
 
     public void ButtonClickKinger(Image image)
     {
-        _imagePanel.sprite = image.sprite;
-        _allPersons.gameObject.SetActive(false);
-        _panel.gameObject.SetActive(true);
-        _audioSource.clip = GetRundomAudio(_audioKinger);
-        _audioSource.Play();
-        _countADS++;
+        if (IsChanceHorror())
+        {
+            ShowHorrorScene(_kingerHorror);
+        }
+        else
+        {
+            ShowCharacterImage(image);
+            PlayAudioCharacter(_audioKinger);
+        }
+
+        _counterADS();
     }
 
     public void ButtonClickPomni(Image image)
     {
-        _imagePanel.sprite = image.sprite;
-        _allPersons.gameObject.SetActive(false);
-        _panel.gameObject.SetActive(true);
-        _audioSource.clip = GetRundomAudio(_audioPomni);
-        _audioSource.Play();
-        _countADS++;
+        if (IsChanceHorror())
+        {
+            ShowHorrorScene(_pomniHorror);
+        }
+        else
+        {
+            ShowCharacterImage(image);
+            PlayAudioCharacter(_audioPomni);
+        }
+
+        _counterADS();
     }
 
     public void ButtonClickRagatha(Image image)
     {
-        _imagePanel.sprite = image.sprite;
-        _allPersons.gameObject.SetActive(false);
-        _panel.gameObject.SetActive(true);
-        _audioSource.clip = GetRundomAudio(_audioRagatha);
-        _audioSource.Play();
-        _countADS++;
+        if (IsChanceHorror())
+        {
+            ShowHorrorScene(_ragathaHorror);
+        }
+        else
+        {
+            ShowCharacterImage(image);
+            PlayAudioCharacter(_audioRagatha);
+        }
+
+        _counterADS();
     }
 
     public void ButtonClickZooble(Image image)
     {
-        _imagePanel.sprite = image.sprite;
-        _allPersons.gameObject.SetActive(false);
-        _panel.gameObject.SetActive(true);
-        _audioSource.clip = GetRundomAudio(_audioZooble);
-        _audioSource.Play();
-        _countADS++;
+        if (IsChanceHorror())
+        {
+            ShowHorrorScene(_zoobleHorror);
+        }
+        else
+        {
+            ShowCharacterImage(image);
+            PlayAudioCharacter(_audioZooble);
+        }
+
+        _counterADS();
     }
 
-    public void ButonExit()
+    public void ButtonExit()
     {
+        int triggerADS = 5;
+
         _allPersons.gameObject.SetActive(true);
         _panel.gameObject.SetActive(false);
         _audioSource.Stop();
 
-        if (_countADS == 5)
+        if (_countADS == triggerADS)
         {
             YandexGame.FullscreenShow();
             _countADS = 0;
         }
+    }
+
+    private void ShowHorrorScene(Image image)
+    {
+        ShowCharacterImage(image);
+        PlayAudioCharacter(_audioHorror);
+    }
+
+    private bool IsChanceHorror()
+    {
+        bool isChance = false;
+        int maxPercent = 4;
+        int minPercent = 1;
+
+        int currrentPercent = Random.Range(minPercent, maxPercent);
+
+        if (currrentPercent == minPercent)
+            isChance = true;
+
+        return isChance;
+    }
+
+    private void PlayAudioCharacter(AudioClip[] audioClips)
+    {
+        _audioSource.clip = GetRundomAudio(audioClips);
+        _audioSource.Play();
+    }
+
+    private void ShowCharacterImage(Image image)
+    {
+        _imagePanel.sprite = image.sprite;
+        _allPersons.gameObject.SetActive(false);
+        _panel.gameObject.SetActive(true);
     }
 
     private AudioClip GetRundomAudio(AudioClip[] audioClips)
@@ -120,4 +198,6 @@ public class Controller : MonoBehaviour
         int randomNumber = Random.Range(0, audioClips.Length);
         return audioClips[randomNumber];
     }
+
+    private int _counterADS() => _countADS++;
 }
